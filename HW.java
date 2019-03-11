8b33458e4d810a1d6ada74018aeef8234aac875d
wyao1 <wyao1@stuy.edu>
2019-03-11 07:29:21 -0400

    wrote the important part, fixing so it compiles

--- HW.java
+++ HW.java
@@ -1,9 +1,25 @@
 import java.util.*;
 public class HW{
-  int partition ( int [] data, int start, int end){
+  public void int partition ( int [] data, int start, int end){
     int pivot = Math.random() * data.length
   }
-  int old = data[0]; //pivot swaps with int[0]
+  int old = data[0]; //pivot swaps with first number
   data[0] = pivot;
   data[pivot] = old;
+  pivot = 0;
+  int start = 1; //indexes for start and end
+  int end = data.length-1;
+  while (start != end){
+    if (data[start] < data[pivot]){
+      data[pivot] = old; //swap smaller one with pivot
+      int oldTwo = data[start];
+      data[start] = old;
+      data[pivot] = knew;
+      pivot++;
+      start++;
+    }
+    else{
+      start++;
+    }
+  }
 }
