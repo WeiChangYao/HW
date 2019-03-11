48fae963be12fc58f9f07075b568946b6da4a075
wyao1 <wyao1@stuy.edu>
2019-03-11 07:07:46 -0400

    added swapping part

--- HW.java
+++ HW.java
@@ -3,4 +3,7 @@ public class HW{
   int partition ( int [] data, int start, int end){
     int pivot = Math.random() * data.length
   }
+  int old = data[0]; //pivot swaps with int[0]
+  data[0] = pivot;
+  data[pivot] = old;
 }
