71e359a57f4583dc6519bc46e8daf04b10e26156
wyao1 <wyao1@stuy.edu>
2019-03-11 23:04:43 -0400

    added swap method and changed some other things

--- Partition.java
+++ Partition.java
@@ -1,17 +1,37 @@
 import java.util.Arrays;
 import java.util.Random;
 public class Partition{
+ public static void swap (int [] dayta, int x, int y){
+  int old = dayta[x];
+  dayta[x] = dayta[y];
+  dayta[y] = old;
+ }
  public static int partition ( int [] data, int start, int end){
   Random r = new Random();
   int pivot = r.nextInt(end-start+1)+start;
   int old = data[start]; //pivot swaps with start number
   data[start] = data[pivot];
   data[pivot] = old;
-  int startOld = start;
-  pivot = startOld; //pivot index is the original start index
-  start = pivot;
-  while (start != end){
-    if (data[start] < data[pivot]){
+  pivot = start; //pivot index is the original start index
+  start++;
+  while(start!=end){
+    if (data[pivot] < data[start]){
+      swap(data, start, end);
+      end--;
+    }
+    else{
+      pivot++;
+      start++;
+    }
+  }
+  if (data[pivot] < data[start]){
+    swap(data, start, pivot);
+    pivot++;
+    start--;
+    end--;
+  }
+  /*while (start != end){
+    if (data[start] > data[pivot]){
       data[pivot] = old; //swap smaller one with pivot
       int oldTwo = data[start];
       data[start] = old;
@@ -23,11 +43,12 @@ public class Partition{
       start++;
     }
   }
+  return pivot;*/
   return pivot;
 }
   public static void main(String[]args){
-    int[] ary = new int[]{0,7,8,9,4};
-    partition(ary,0,4);
-    System.out.println(Arrays.toString(ary));
+    int[] data1 = {10, 80, 30, 90, 40, 50, 70};
+    partition(data1,0,6);
+    System.out.println(Arrays.toString(data1));
   }
 }
