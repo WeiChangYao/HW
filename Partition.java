a8f1cfbddbf2c4527f933cec2f34a84fc0ef47d1
wyao1 <wyao1@stuy.edu>
2019-03-11 21:40:08 -0400

    added driver to test partition

--- Partition.java
+++ Partition.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 import java.util.Random;
-public class HW{
- public void partition ( int [] data, int start, int end){
+public class Partition{
+ public static int partition ( int [] data, int start, int end){
   Random r = new Random();
   int pivot = r.nextInt(end-start)+start;
   int old = data[0]; //pivot swaps with first number
-  data[0] = pivot;
+  data[0] = data[pivot];
   data[pivot] = old;
   pivot = 0;
   //int start = 1; //indexes for start and end
@@ -23,11 +23,11 @@ public class HW{
       start++;
     }
   }
+  return pivot;
 }
   public static void main(String[]args){
-    int[] ary = new int[5];
-    ary = int[4,2,3,1,6]
+    int[] ary = new int[]{0,7,8,9,4};
     partition(ary,0,4);
-    System.out.println(ary);
+    System.out.println(Arrays.toString(ary));
   }
 }
