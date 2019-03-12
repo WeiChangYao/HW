d06aad5b340786d383289ce1a131b222835abab2
wyao1 <wyao1@stuy.edu>
2019-03-11 22:01:28 -0400

    fixed pivot starting position and swap

--- Partition.java
+++ Partition.java
@@ -3,13 +3,13 @@ import java.util.Random;
 public class Partition{
  public static int partition ( int [] data, int start, int end){
   Random r = new Random();
-  int pivot = r.nextInt(end-start)+start;
-  int old = data[0]; //pivot swaps with first number
-  data[0] = data[pivot];
+  int pivot = r.nextInt(end-start+1)+start;
+  int old = data[start]; //pivot swaps with start number
+  data[start] = data[pivot];
   data[pivot] = old;
-  pivot = 0;
-  //int start = 1; //indexes for start and end
-  //int end = data.length-1;
+  int startOld = start;
+  pivot = startOld; //pivot index is the original start index
+  start = pivot;
   while (start != end){
     if (data[start] < data[pivot]){
       data[pivot] = old; //swap smaller one with pivot
